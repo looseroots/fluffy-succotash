a25dba283cbc9e368b93a669365df0ff2641cf6f
Jack Newsom <jackmnewsom@gmail.com>
2018-07-14 20:07:43 -0700

    added changes

--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
 from flask import (redirect, url_for, render_template, flash, g, request)
 from app import app
+from app.forms import HomeForm
 import os
+import random
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    home_form = HomeForm()
+    if home_form.validate_on_submit():
+        return "Form submission successful!"
+
+    return render_template(
+        'index.html',
+        navbar_emoji=random.choice(app.config['NAVBAR_EMOJIS']),
+        home_form=home_form
+    )
