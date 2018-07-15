1838703b616c5bc7e2749de61c3afa326c1f00ff
Jack Newsom <jackmnewsom@gmail.com>
2018-07-14 22:18:40 -0700

    Added form stuff

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, DateTimeField, IntegerField
 from wtforms.validators import DataRequired
 
 class HomeForm(FlaskForm):
-    location = StringField('location', validators=[DataRequired()])
+    start_location = StringField('Starting Location',
+        validators=[DataRequired()])
+    end_location = StringField('Ending Location',
+        validators=[DataRequired()])
+    start_time = DateTimeField('Start Time',
+        format='%H:%M:%S',
+        validators=[DataRequired()])
+    end_time = DateTimeField('End Time',
+        format='%H:%M:%S',
+        validators=[DataRequired()])
+    budget = IntegerField('Story Budget')
--- app/views.py
+++ app/views.py
@@ -1,17 +1,34 @@
-from flask import (redirect, url_for, render_template, flash, g, request)
+from flask import (redirect, url_for, render_template, flash, g, request, session)
 from app import app
 from app.forms import HomeForm
 import os
 import random
+import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     home_form = HomeForm()
     if home_form.validate_on_submit():
-        return "Form submission successful!"
+        home_form = [home_form.start_location.data,home_form.end_location.data, home_form.start_time.data, home_form.end_time.data]
+        home_form = [str(field) for field in home_form]
+        session['home_form'] = home_form
+        print(session['home_form'])
+        return redirect(url_for('planner'))
 
     return render_template(
         'index.html',
         navbar_emoji=random.choice(app.config['NAVBAR_EMOJIS']),
         home_form=home_form
+    )
+
+@app.route('/planner', methods=['GET', 'POST'])
+def planner():
+    print(session['home_form'])
+
+    return render_template(
+        'planner.html',
+        start_location=session['home_form'][0],
+        end_location=session['home_form'][1],
+        start_time=session['home_form'][2],
+        end_time=session['home_form'][3]
     )
