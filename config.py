810227380b1b692b101acc051462ab1c46d99562
Cody Swain <swain.cody@gmail.com>
2018-07-28 18:26:08 -0700

    Google OAuth Login

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 NAVBAR_EMOJIS = ['⛷', '🏂', '⛹️‍♂️', '⛹️‍♂️', '🧗‍♀️', '🚵‍♀️', '🎪', '🎭',
 '🎬', '🎳', '🛩', '🚀', '🛶', '⛵️','🏟','🎡','🎢', '🏝','🏜','🥞','🥓']
 SECRET_KEY = 'rUbf2Dh6uE'
+
+GOOGLE_ID = '936705119130-vi6cb4jbnugngf5l3qkb3dnr55loccj4.apps.googleusercontent.com'
+GOOGLE_SECRET = 'gCgBFqBBAOWAv_j5ErOjWd1k'
--- ppa.py
+++ ppa.py
@@ -1,6 +1,8 @@
 from app import app
 import argparse
 
+from flask_oauthlib.client import OAuth
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug', dest='debug', action='store_true')
 
@@ -9,5 +11,9 @@ debug = parser.parse_args().debug
 if debug:
     # disable static file caching in debug
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+    #app.debug = True 
+# if __name__ == "__main__":
+	#app.run()
 
 app.run(debug=parser.parse_args().debug)
